30198a34d8256360f43792baea385cd6e811a899
amir260101~ <amir26hossin@gmail.com>
2023-05-02 18:44:48 +0600

    error page done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,34 +8,37 @@ import Home from '../pages/Home/Home/Home';
 import Blog from './../pages/Home/Shared/Blog/Blog';
 import Chef from '../layout/Chef';
 import ChefDetails from '../pages/ChefDetails/ChefDetails';
+import Error from '../pages/Error/Error';
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: "/",
-            element: <Home></Home>,
-            loader: () => fetch(`http://localhost:5000/chef`)
-        },
-        {
-            path: "/blog",
-            element: <Blog></Blog>
-        }
-      ]
+        path: "/",
+        element: <Main></Main>,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+                loader: () => fetch(`http://localhost:5000/chef`)
+            },
+            {
+                path: "/blog",
+                element: <Blog></Blog>
+            }
+        ]
     },
     {
         path: "/chef",
         element: <Chef></Chef>,
+        errorElement: <Error></Error>,
         children: [
             {
                 path: ":id",
                 element: <ChefDetails></ChefDetails>,
-                loader: ({params}) => fetch(`http://localhost:5000/chef/${params.id}`)
+                loader: ({ params }) => fetch(`http://localhost:5000/chef/${params.id}`)
             }
         ]
     }
-  ]);
+]);
 
 export default router;
